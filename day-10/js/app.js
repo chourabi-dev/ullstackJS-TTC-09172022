67a48bc84b2085275dcdc74ed23422a53980cb7e
chourabi-dev <chourabitahers62@gmail.com>
2022-11-05 10:10:31 +0100

    latest

---
+++ day-10/js/app.js
@@ -0,0 +1,7 @@
+var productsList = [
+    { id:1 , title:"Iphone 14", price:5800 , photoURL:"https://tdiscount.tn/8-large_default/apple-iphone-14-black.webp" },
+    { id:2 , title:"Sony playstation 2", price:250 , photoURL:"https://upload.wikimedia.org/wikipedia/commons/1/1c/PS2-Versions.jpg" },
+    { id:3 , title:"Lg smart tv", price:2300 , photoURL:"https://m.media-amazon.com/images/I/71IBre-QupL._SL1500_.jpg" },
+    { id:4 , title:"Samsung s21", price:2800 , photoURL:"https://www.tunisianet.com.tn/188886-large/telephone-portable-samsung-galaxy-s21-5g-gris.jpg" },
+    
+];
